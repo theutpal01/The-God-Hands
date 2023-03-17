1254a999a2a03e4076f5a8aa288e25caac7c6f2f
Utpal <theutpal01@gmail.com>
2023-03-17 09:29:56 +0530

    Files

--- VideoGet.py
+++ VideoGet.py
@@ -4,8 +4,8 @@ import cv2
 class VideoGet:
     def __init__(self, win_size, src=0):
         self.stream = cv2.VideoCapture(src)
-        cv2.namedWindow("Viewer", cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_AUTOSIZE)
-        cv2.resizeWindow("Viewer", win_size[0], win_size[1])
+        cv2.namedWindow("The Invisible Hands", cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_AUTOSIZE)
+        cv2.resizeWindow("The Invisible Hands", win_size[0], win_size[1])
         self.grabbed, self.frame = self.stream.read()
 
 
@@ -14,5 +14,5 @@ class VideoGet:
 
     
     def showWin(self, frame):
-        cv2.imshow("Viewer", frame)
+        cv2.imshow("The Invisible Hands", frame)
 
